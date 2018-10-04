9f635c4e60695acd582eca8e417eea665cdb4d1c
sergeymaguire <sergeymaguire@gmail.com>
2018-10-04 13:03:20 -0700

    homework done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,53 +1,204 @@
-$("#find-movie").on("click", function (event) {
+//Step 1. geocoder is loaded in initMap function, to be used later to get the zip or nearest zip code of the current user
+//Step 2. gets the current lat and long with the built in navigator(the browser object) function
+//Step 3. get the first zip code found in the nearby locations of the current user
+//Put on global level to be used later in function(Get zip code)
+//This is called from the index.html script link
+var geocoder;
+function initMap() {
+  geocoder = new google.maps.Geocoder(); // sets geocoder
+}
+$(document).ready(function() {
+  var lat, long, zip; //these are set and used
+  //Step 2. gets the current lat and long with the built in navigator(the browser object) function
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position);
+    lat = position.coords.latitude; // sets global lat variable used in getzipcode function
+    lng = position.coords.longitude; //sets global long variable used in getzipcode function
+    getZipCode(lat, lng);
+  });
 
-    var config = {
-        apiKey: "AIzaSyBXh_OSGPOTI4ZxdxcJL5dcV3oByDTTVwc",
-        authDomain: "andchill-eb480.firebaseapp.com",
-        databaseURL: "https://andchill-eb480.firebaseio.com",
-        projectId: "andchill-eb480",
-        storageBucket: "andchill-eb480.appspot.com",
-        messagingSenderId: "198184378958"
+  function getZipCode(lat, long) {
+    var latlng = {
+      lat: lat,
+      lng: long
     };
-    firebase.initializeApp(config);
-    var database = firebase.database();
- 
-    event.preventDefault();
- 
-    var movie = $("#movie-input").val();
-    if (!movie) return;
- 
-    var queryURL = "https://utelly-tv-shows-and-movies-availability-v1.p.mashape.com/lookup?";
- 
-    var queryString = queryURL + "country=" + "us"  + "&term=" + movie;
-    getMovies(queryString);
- 
-    // -----------------------------------------------------------------------
- 
- });
- 
- function getMovies(queryURL) {
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        headers: {
-            'X-Mashape-Key': 'dEFISQiTvwmshEJAWU2KwJRqazW0p1I0lb0jsn5LUpy7owpPJ6',
-            Accept: 'application/json'
+    showLoading();
+    geocoder.geocode(
+      //uses the global geocoder variable object
+      {
+        location: latlng
+      },
+      function(results, status) {
+        if (status === "OK") {
+          if (results[0]) {
+            for (j = 0; j < results[0].address_components.length; j++) {
+              if (results[0].address_components[j].types[0] == "postal_code") {
+                zip = results[0].address_components[j].short_name;
+                //above is waiting for the zipcode to return before setting up the click events
+                zipCodeReady();
+              }
+            }
+          }
         }
-    }).then(function (response) {
-        if ($.isArray(response.results) && response.results.length) {
-            var html = buildHtml(response.results);
-            $("#movies").empty();
-            $("#movies").append(html);
-        };
+        hideLoading();
+      }
+    );
+  }
+
+  var config = {
+    apiKey: "AIzaSyBXh_OSGPOTI4ZxdxcJL5dcV3oByDTTVwc",
+    authDomain: "andchill-eb480.firebaseapp.com",
+    databaseURL: "https://andchill-eb480.firebaseio.com",
+    projectId: "andchill-eb480",
+    storageBucket: "andchill-eb480.appspot.com",
+    messagingSenderId: "198184378958"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+  function zipCodeReady() {
+    $("#find-movie").on("click", getRestaurantsFromYelp); //going to call getfood function on the click event
+    $("#find-movie").on("click", movieSearch); //Can not execute these until the zip code is returned in the above function where zipCodeReady() is being called.
+  }
+
+  function movieSearch(event) {
+    var movieSearch = $("#movie-input")
+      .val()
+      .trim();
+
+    event.preventDefault();
+
+    if (!movieSearch) return;
+    var queryURL =
+      "https://utelly-tv-shows-and-movies-availability-v1.p.mashape.com/lookup?";
+    var queryString = queryURL + "country=" + "us" + "&term=" + movieSearch;
+    getMovies(queryString);
+  }
+
+  function getMovies(queryURL) {
+    console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      headers: {
+        "X-Mashape-Key": "dEFISQiTvwmshEJAWU2KwJRqazW0p1I0lb0jsn5LUpy7owpPJ6",
+        Accept: "application/json"
+      }
+    }).success(function(response) {
+      console.log(response);
+      $("#movies").empty();
+      if ($.isArray(response.results) && response.results.length) {
+        var html = buildMovieHtml(response.results);
+        $("#movies").append(html);
+        addMoviesToFirebase(response.results);
+      }
     });
- };
- 
- function buildHtml(results) {
-    var html = "<ul>";
+  }
+
+  function buildMovieHtml(results) {
+    var html = " <div class='row text-center text-lg-left'>";
+
     for (var i = 0; i < results.length; i++) {
-        html = html + "<li>" + "<img src='" + results[i].picture +  "'<p>" + results[0].name + "</p>'" + "<a href=" + results[0].locations[0].url + ">click here</a>"
-        "'></li>";
-    };
-    html = html + "</ul>";
+      if (results[i].picture) {
+        html =
+          html +
+          "<div class='col-lg-3 col-md-4 col-xs-6'>" +
+          "<img src='" +
+          results[i].picture +
+          "'><div>" +
+          results[i].name +
+          "</div>Showing at: " +
+          getLocationsHtml(results[i].locations) +
+          "</div>";
+      } else {
+        html =
+          html +
+          "<div class='col-lg-3 col-md-4 col-xs-6'>" +
+          "<div>" +
+          results[i].name +
+          "</div>Showing at: " +
+          getLocationsHtml(results[i].locations) +
+          "</div>";
+      }
+    }
+    html = html + "</div>";
     return html;
- }
+  }
+
+  function getLocationsHtml(locations) {
+    var html = "";
+    for (var i = 0; i < locations.length; i++) {
+      html =
+        html +
+        "<a  target='blank' href=" +
+        locations[i].url +
+        ">" +
+        "<br>" +
+        locations[i].display_name +
+        "</a>";
+    }
+    return html;
+  }
+
+  function addMoviesToFirebase(results) {
+    for (var i = 0; i < results.length; i++) {
+      database.ref().push({
+        name: results[i].name,
+        FindOn: results[i].locations[0].display_name,
+        Image: results[i].picture
+      });
+    }
+  }
+  function showLoading() {
+    //shows the loading icon when waiting for movies or restaurants to return
+    $(".loader").show();
+  }
+  function hideLoading() {
+    //hides the loading icon when the movies or restaurants return
+    $(".loader").hide();
+  }
+  function getRestaurantsFromYelp() {
+    if (!zip) {
+      alert("No zip code");
+      return;
+    }
+    var queryGURL =
+      "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+      lat +
+      "," +
+      lng +
+      "&key=AIzaSyDB8oKKFdugWo4hKPNthhL1ouMovHmJ_Is";
+    showLoading();
+    $.ajax({
+      url: queryGURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response.results[4].address_components[0].long_name);
+      var YqueryURL = "https://dl-yelp-help.herokuapp.com/yelp?zip=" + zip;
+      $.ajax({
+        url: YqueryURL,
+        method: "GET"
+      }).then(function(response) {
+        for (var i = 0; i < response.length; i++) {
+          var html = "";
+          html =
+            html +
+            "<p id='name'>" +
+            response[i].name +
+            "</p>" +
+            "<p id='address'>" +
+            response[i].location.address1 +
+            "</p>" +
+            "<p id='phone'>" +
+            response[i].display_phone +
+            "</p>" +
+            "<p id='price'>" +
+            response[i].price +
+            "</p>";
+          $("#restaurant").append(html);
+        }
+        hideLoading();
+      });
+    });
+  }
+});
